d2afe327f4df272819a6e424f06905a9f1b1e88f
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-07-15 15:23:01 -0400

    fix win_print_select_file() causing segfault when lacking file permissions

--- src/joshuto.rs
+++ src/joshuto.rs
@@ -62,7 +62,7 @@ mod joshuto_rwx {
     }
 }
 
-pub mod joshuto_sort {
+mod joshuto_sort {
 
     use std::cmp;
     use std::fs;
@@ -92,225 +92,6 @@ pub mod joshuto_sort {
         }
         res_ordering(file1, file2).unwrap_or(cmp::Ordering::Less)
     }
-}
-
-pub fn run(config : &JoshutoConfig)
-{
-    init_ncurses();
-
-    let mut term_rows : i32 = 0;
-    let mut term_cols : i32 = 0;
-    ncurses::getmaxyx(ncurses::stdscr(), &mut term_rows, &mut term_cols);
-
-    let mut index : usize = 0;
-    let mut pindex : usize = 0;
-    // let mut cindex : usize = 0;
-
-    ncurses::refresh();
-
-    let mut top_win = ncurses::newwin(1, term_cols, 0, 0);
-    let mut mid_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
-                                        1, term_cols / 7);
-    let mut left_win = ncurses::newwin(term_rows - 2, term_cols / 7, 1, 0);
-    let mut right_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
-                                        1, term_cols / 7 * 4);
-    let mut bottom_win = ncurses::newwin(1, term_cols, term_rows - 1, 0);
-
-    // ncurses::scrollok(mid_win, true);
-
-    /* TODO: mutable in the future */
-    let sort_func : fn(file1 : &std::fs::DirEntry, file2 : &std::fs::DirEntry) -> std::cmp::Ordering
-        = joshuto_sort::alpha_sort;
-
-    let mut dir_contents : Vec<fs::DirEntry>; // = Vec::new();
-    match cwd_contents() {
-        Ok(s) => {
-            dir_contents = s;
-            dir_contents.sort_by(sort_func);
-        }
-        Err(_e) => {
-            process::exit(1);
-        }
-    }
-
-    win_print_curr_path(top_win);
-    win_print_curr_path(bottom_win);
-    win_print_parent_dir(left_win, pindex, (term_rows - 1) as usize);
-
-    win_contents_refresh_indexed(mid_win, &dir_contents, (term_rows - 1) as usize, index);
-
-    if dir_contents.len() > 0 {
-        win_print_select_file(right_win, &dir_contents[index], (term_rows - 1) as usize);
-        win_print_file_info(bottom_win, &dir_contents[index]);
-    }
-
-    ncurses::refresh();
-
-    loop {
-        let ch = ncurses::getch();
-
-        match ch {
-            QUIT => {
-                break;
-            }
-            ncurses::KEY_RESIZE => {
-                ncurses::getmaxyx(ncurses::stdscr(), &mut term_rows, &mut term_cols);
-
-                ncurses::delwin(top_win);
-                ncurses::delwin(mid_win);
-                ncurses::delwin(left_win);
-                ncurses::delwin(right_win);
-                ncurses::delwin(bottom_win);
-
-
-                top_win = ncurses::newwin(1, term_cols, 0, 0);
-                mid_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
-                                          term_cols / 7, 1);
-                left_win = ncurses::newwin(term_rows - 2, term_cols / 7, 0, 1);
-                right_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
-                                            term_cols / 7 * 4, 1);
-                bottom_win = ncurses::newwin(1, term_cols, term_rows - 1, 0);
-                ncurses::refresh();
-            }
-            ncurses::KEY_UP => {
-                if index > 0 {
-                    index = index - 1;
-                    win_print_select_file(right_win, &dir_contents[index], (term_rows - 1) as usize);
-                }
-            }
-            ncurses::KEY_DOWN => {
-                if index + 1 < dir_contents.len() {
-                    index = index + 1;
-                    win_print_select_file(right_win, &dir_contents[index], (term_rows - 1) as usize);
-                }
-            }
-            ncurses::KEY_LEFT => {
-                if let Ok(mut pathbuf) = env::current_dir() {
-                    if pathbuf.eq(&path::Path::new("/")) {
-                        ncurses::wclear(left_win);
-                        ncurses::wrefresh(left_win);
-                        continue;
-                    }
-                    if pathbuf.pop() == false {
-                        continue;
-                    }
-                    match env::set_current_dir(&pathbuf) {
-                        Ok(_) => {
-                            if let Ok(tmp_contents) = cwd_contents() {
-                                dir_contents = tmp_contents;
-                                dir_contents.sort_by(sort_func);
-
-                                index = pindex;
-                                pindex = 0;
-
-                                if pathbuf.eq(&path::Path::new("/")) {
-                                    ncurses::wclear(left_win);
-                                    ncurses::wrefresh(left_win);
-                                } else {
-                                    win_print_curr_path(top_win);
-                                    win_print_parent_dir(left_win, pindex,
-                                        (term_rows - 1) as usize);
-                                }
-                                win_print_select_file(right_win,
-                                    &dir_contents[index],
-                                    (term_rows - 1) as usize);
-                            }
-                        },
-                        Err(e) => {
-                            win_print_err_msg(bottom_win, format!("{}", e).as_str());
-                        },
-                    };
-                }
-            }
-            ncurses::KEY_RIGHT => {
-                match dir_contents[index as usize].file_type() {
-                    Ok(file_type) => {
-                        if file_type.is_dir() || file_type.is_symlink() {
-                            let new_path : path::PathBuf = dir_contents[index as usize].path();
-                            match env::set_current_dir(new_path) {
-                                Ok(_s) => {
-                                    match cwd_contents() {
-                                        Ok(s) => {
-                                            dir_contents = s;
-                                            dir_contents.sort_by(sort_func);
-                                        }
-                                        Err(_e) => {
-                                            process::exit(1);
-                                        }
-                                    }
-                                    pindex = index;
-                                    index = 0;
-
-                                    win_print_curr_path(top_win);
-                                    win_print_parent_dir(left_win, pindex,
-                                        (term_rows - 1) as usize);
-                                    if dir_contents.len() > 0 {
-                                        win_print_select_file(right_win,
-                                            &dir_contents[index],
-                                            (term_rows - 1) as usize);
-                                    }
-                                },
-                                Err(e) => {
-                                    win_print_err_msg(bottom_win, format!("{}", e).as_str());
-                                },
-                            };
-                        }
-                    },
-                    Err(_e) => {
-                        ncurses::printw("None");
-                    },
-                };
-            }
-            ENTER => {
-                match dir_contents[index as usize].file_type() {
-                    Ok(file_type) => {
-                        if file_type.is_dir() || file_type.is_symlink() {
-                            let new_path : path::PathBuf = dir_contents[index as usize].path();
-                            match env::set_current_dir(new_path) {
-                                Ok(_s) => {
-                                    match cwd_contents() {
-                                        Ok(s) => {
-                                            dir_contents = s;
-                                            dir_contents.sort_by(sort_func);
-                                        }
-                                        Err(_e) => {
-                                            process::exit(1);
-                                        }
-                                    }
-                                    pindex = index;
-                                    index = 0;
-
-                                    win_print_curr_path(top_win);
-                                    win_print_parent_dir(left_win, pindex,
-                                        (term_rows - 1) as usize);
-                                    if dir_contents.len() > 0 {
-                                        win_print_select_file(right_win,
-                                            &dir_contents[index],
-                                            (term_rows - 1) as usize);
-                                    }
-                                },
-                                Err(_e) => {
-                                    ncurses::printw("None");
-                                },
-                            };
-                        }
-                    },
-                    Err(_e) => {
-                        ncurses::printw("None");
-                    },
-                };
-            }
-            _ => {
-                    ncurses::wprintw(mid_win, format!("pressed: {}\n",
-			            std::char::from_u32(ch as u32).expect("Invalid char")).as_ref());
-            }
-        };
-
-        win_contents_refresh_indexed(mid_win, &dir_contents,
-                                            (term_rows - 1) as usize, index);
-        win_print_file_info(bottom_win, &dir_contents[index]);
-    }
-    ncurses::endwin();
 }
 
 pub fn init_ncurses()
@@ -526,19 +307,22 @@ pub fn win_print_parent_dir(win : ncurses::WINDOW, index : usize, length : usize
 pub fn win_print_select_file(win : ncurses::WINDOW, file : &fs::DirEntry, length : usize)
 {
     ncurses::wclear(win);
-    match file.metadata() {
-        Ok(metadata) => {
-            if metadata.is_dir() {
-                let tmp_result : Result<Vec<fs::DirEntry>, _> = fs::read_dir(&file.path()).unwrap().collect();
-                let mut tmp_cdir : Vec<fs::DirEntry> = tmp_result.unwrap();
-                tmp_cdir.sort_by(joshuto_sort::alpha_sort);
-                win_contents_refresh(win, &tmp_cdir, length);
-            }
-        }
-        Err(_e) => {
-            ncurses::wattron(win, ncurses::COLOR_PAIR(3));
-            ncurses::wprintw(win, "Error");
-            ncurses::wattroff(win, ncurses::COLOR_PAIR(3));
+    if let Ok(metadata) = file.metadata() {
+        if metadata.is_dir() {
+            match fs::read_dir(&file.path()) {
+                Ok(results) => {
+                    let results : Result<Vec<fs::DirEntry>, _> = results.collect();
+                    if let Ok(mut dir_contents) = results {
+                        dir_contents.sort_by(joshuto_sort::alpha_sort);
+                        win_contents_refresh(win, &dir_contents, length);
+                    }
+                },
+                Err(e) => {
+                    ncurses::wattron(win, ncurses::COLOR_PAIR(3));
+                    win_print_err_msg(win, format!("{}", e).as_str());
+                    ncurses::wattroff(win, ncurses::COLOR_PAIR(3));
+                },
+            };
         }
     }
     ncurses::wrefresh(win);
@@ -583,4 +367,177 @@ pub fn win_print_err_msg(win : ncurses::WINDOW, err_msg : &str)
     ncurses::wclear(win);
     ncurses::mvwprintw(win, 0, 0, err_msg);
     ncurses::wrefresh(win);
+}
+
+pub fn run(config : &JoshutoConfig)
+{
+    init_ncurses();
+
+    let mut term_rows : i32 = 0;
+    let mut term_cols : i32 = 0;
+    ncurses::getmaxyx(ncurses::stdscr(), &mut term_rows, &mut term_cols);
+
+    let mut index : usize = 0;
+    let pindex : usize = 0;
+    // let mut cindex : usize = 0;
+
+    ncurses::refresh();
+
+    let mut top_win = ncurses::newwin(1, term_cols, 0, 0);
+    let mut mid_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
+                                        1, term_cols / 7);
+    let mut left_win = ncurses::newwin(term_rows - 2, term_cols / 7, 1, 0);
+    let mut right_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
+                                        1, term_cols / 7 * 4);
+    let mut bottom_win = ncurses::newwin(1, term_cols, term_rows - 1, 0);
+
+    // ncurses::scrollok(mid_win, true);
+
+    /* TODO: mutable in the future */
+    let sort_func : fn(file1 : &std::fs::DirEntry, file2 : &std::fs::DirEntry) -> std::cmp::Ordering
+        = joshuto_sort::alpha_sort;
+
+    let mut dir_contents : Vec<fs::DirEntry>; // = Vec::new();
+    match cwd_contents() {
+        Ok(s) => {
+            dir_contents = s;
+            dir_contents.sort_by(sort_func);
+        }
+        Err(_e) => {
+            process::exit(1);
+        }
+    }
+
+    win_print_curr_path(top_win);
+    win_print_curr_path(bottom_win);
+    win_print_parent_dir(left_win, pindex, (term_rows - 1) as usize);
+
+    win_contents_refresh_indexed(mid_win, &dir_contents, (term_rows - 1) as usize, index);
+
+    if dir_contents.len() > 0 {
+        win_print_select_file(right_win, &dir_contents[index], (term_rows - 1) as usize);
+        win_print_file_info(bottom_win, &dir_contents[index]);
+    }
+
+    ncurses::refresh();
+
+    loop {
+        let ch = ncurses::getch();
+
+        match ch {
+            QUIT => {
+                break;
+            }
+            ncurses::KEY_RESIZE => {
+                ncurses::getmaxyx(ncurses::stdscr(), &mut term_rows, &mut term_cols);
+
+                ncurses::delwin(top_win);
+                ncurses::delwin(mid_win);
+                ncurses::delwin(left_win);
+                ncurses::delwin(right_win);
+                ncurses::delwin(bottom_win);
+
+
+                top_win = ncurses::newwin(1, term_cols, 0, 0);
+                mid_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
+                                          term_cols / 7, 1);
+                left_win = ncurses::newwin(term_rows - 2, term_cols / 7, 0, 1);
+                right_win = ncurses::newwin(term_rows - 2, term_cols / 7 * 3,
+                                            term_cols / 7 * 4, 1);
+                bottom_win = ncurses::newwin(1, term_cols, term_rows - 1, 0);
+                ncurses::refresh();
+            }
+            ncurses::KEY_UP => {
+                if index > 0 {
+                    index = index - 1;
+                    win_print_select_file(right_win, &dir_contents[index], (term_rows - 1) as usize);
+                }
+            }
+            ncurses::KEY_DOWN => {
+                if index + 1 < dir_contents.len() {
+                    index = index + 1;
+                    win_print_select_file(right_win, &dir_contents[index], (term_rows - 1) as usize);
+                }
+            }
+            ncurses::KEY_LEFT => {
+                if let Ok(mut pathbuf) = env::current_dir() {
+                    if pathbuf.eq(&path::Path::new("/")) {
+                        ncurses::wclear(left_win);
+                        ncurses::wrefresh(left_win);
+                        continue;
+                    }
+                    if pathbuf.pop() == false {
+                        continue;
+                    }
+                    match env::set_current_dir(&pathbuf) {
+                        Ok(_) => {
+                            if let Ok(tmp_contents) = cwd_contents() {
+                                dir_contents = tmp_contents;
+                                dir_contents.sort_by(sort_func);
+
+                                index = pindex;
+
+                                if pathbuf.eq(&path::Path::new("/")) {
+                                    ncurses::wclear(left_win);
+                                    ncurses::wrefresh(left_win);
+                                } else {
+                                    win_print_curr_path(top_win);
+                                    win_print_parent_dir(left_win, pindex,
+                                        (term_rows - 1) as usize);
+                                }
+                                win_print_select_file(right_win,
+                                    &dir_contents[index],
+                                    (term_rows - 1) as usize);
+                            }
+                        },
+                        Err(e) => {
+                            win_print_err_msg(bottom_win, format!("{}", e).as_str());
+                        },
+                    };
+                }
+            }
+            ncurses::KEY_RIGHT | ENTER => {
+                if let Ok(file_type) = dir_contents[index as usize].file_type() {
+                    if file_type.is_dir() || file_type.is_symlink() {
+                        let new_path : path::PathBuf = dir_contents[index as usize].path();
+                        match env::set_current_dir(new_path) {
+                            Ok(_s) => {
+                                match cwd_contents() {
+                                    Ok(s) => {
+                                        dir_contents = s;
+                                        dir_contents.sort_by(sort_func);
+                                    }
+                                    Err(_e) => {
+                                        process::exit(1);
+                                    }
+                                }
+                                index = 0;
+
+                                win_print_curr_path(top_win);
+                                win_print_parent_dir(left_win, pindex,
+                                    (term_rows - 1) as usize);
+                                if dir_contents.len() > 0 {
+                                    win_print_select_file(right_win,
+                                        &dir_contents[index],
+                                        (term_rows - 1) as usize);
+                                }
+                            },
+                            Err(e) => {
+                                win_print_err_msg(bottom_win, format!("{}", e).as_str());
+                            },
+                        };
+                    }
+                }
+            }
+            _ => {
+                    ncurses::wprintw(mid_win, format!("pressed: {}\n",
+			            std::char::from_u32(ch as u32).expect("Invalid char")).as_ref());
+            }
+        };
+
+        win_contents_refresh_indexed(mid_win, &dir_contents,
+                                            (term_rows - 1) as usize, index);
+        win_print_file_info(bottom_win, &dir_contents[index]);
+    }
+    ncurses::endwin();
 }
