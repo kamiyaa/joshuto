68d8d2ae546e6cbfdc8183bd4b522d75d4f13ef2
Jiayi Zhao <jeff.no.zhao@gmail.com>
2018-07-16 22:30:14 -0400

    add is_executable() function

--- src/joshuto_unix.rs
+++ src/joshuto_unix.rs
@@ -10,6 +10,21 @@ pub const S_IFDIR  : u32 = 0o040000;   /* directory */
 pub const S_IFCHR  : u32 = 0o020000;   /* character device */
 pub const S_IFIFO  : u32 = 0o010000;   /* FIFO */
 
+pub fn is_executable(mode : u32) -> bool
+{
+    const LIBC_PERMISSION_VALS : [(u32, char) ; 3] = [
+            (libc::S_IXUSR, 'x'),
+            (libc::S_IXGRP, 'x'),
+            (libc::S_IXOTH, 'x'),
+        ];
+    for val in LIBC_PERMISSION_VALS.iter() {
+        if mode & val.0 != 0 {
+            return true;
+        }
+    }
+    return false;
+}
+
 pub fn stringify_mode(mode : u32) -> String
 {
     let mut mode_str : String = String::with_capacity(10);
