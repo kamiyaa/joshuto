72f5e1eaf4c0fcbe39bd3f0d80cf9ab45d504057
Jiayi Zhao <jeff.no.zhao@gmail.com>
2020-12-29 22:20:03 -0500

    add back support for different sized columns

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,19 +1,5 @@
-use tui::layout::Constraint;
-
 mod tui_backend;
 pub mod views;
 pub mod widgets;
 
 pub use tui_backend::*;
-
-pub const DEFAULT_LAYOUT: [tui::layout::Constraint; 3] = [
-    Constraint::Ratio(1, 8),
-    Constraint::Ratio(3, 8),
-    Constraint::Ratio(4, 8),
-];
-
-pub const NO_PREVIEW_LAYOUT: [tui::layout::Constraint; 3] = [
-    Constraint::Ratio(1, 8),
-    Constraint::Ratio(7, 8),
-    Constraint::Ratio(0, 8),
-];
