30bfa2fd8411f8e8fc4457e5a1a3f101bd57548f
Daniel Llin Ferrero <daniel@llin.info>
2021-06-06 15:19:14 +0200

    cleanup dead code

--- src/commands/touch_file.rs
+++ src/commands/touch_file.rs
@@ -5,59 +5,7 @@ use crate::error::JoshutoResult;
 use crate::util::load_child::LoadChild;
 use filetime::FileTime;
 use std::fs::File;
-use std::fs::OpenOptions;
 use std::time::SystemTime;
-
-fn _touch_file(context: &mut AppContext, file: &path::Path) -> std::io::Result<()> {
-    match file.parent() {
-        Some(base_path) => {
-            if !base_path.exists() {
-                let err = std::io::Error::new(
-                    std::io::ErrorKind::AlreadyExists,
-                    "Filename already exists",
-                );
-                return Err(err);
-            }
-        }
-        None => {
-            let err =
-                std::io::Error::new(std::io::ErrorKind::AlreadyExists, "Filename already exists");
-            return Err(err);
-        }
-    }
-    match OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open(&file)
-    {
-        Ok(_) => {}
-        Err(_) => {
-            let err =
-                std::io::Error::new(std::io::ErrorKind::AlreadyExists, "Filename already exists");
-            return Err(err);
-        }
-    }
-    //   .map_err(|_| format!("could not open {}", file.to_string_lossy()))?;
-
-    let file_time = FileTime::from_system_time(SystemTime::now());
-
-    match filetime::set_file_times(file, file_time, file_time) {
-        Ok(_) => {}
-        Err(_) => {
-            let err =
-                std::io::Error::new(std::io::ErrorKind::AlreadyExists, "Filename already exists");
-            return Err(err);
-        }
-    }
-    //   .map_err(|_| String::from("could not update file times"))?;
-
-    let options = context.config_ref().display_options_ref().clone();
-    if let Some(curr_list) = context.tab_context_mut().curr_tab_mut().curr_list_mut() {
-        curr_list.reload_contents(&options)?;
-    }
-    Ok(())
-}
 
 fn _update_actime(file: &path::Path) -> std::io::Result<()> {
     let file_time = FileTime::from_system_time(SystemTime::now());
