701f728af722e8cbeca07c0815492aa2bc19484e
Jeff Zhao <jeff.no.zhao@gmail.com>
2021-08-24 17:17:36 -0400

    quit_to_cwd won't work if workers are busy

--- src/commands/quit.rs
+++ src/commands/quit.rs
@@ -22,6 +22,14 @@ pub fn force_quit(context: &mut AppContext) -> JoshutoResult<()> {
 }
 
 pub fn quit_to_current_directory(context: &mut AppContext) -> JoshutoResult<()> {
-    context.quit = QuitType::ToCurrentDirectory;
+    if worker_context.is_busy() || !worker_context.is_empty() {
+        Err(JoshutoError::new(
+            JoshutoErrorKind::Io(io::ErrorKind::Other),
+            String::from("operations running in background, use force_quit to quit"),
+        ))
+    } else {
+        context.quit = QuitType::ToCurrentDirectory;
+        Ok(())
+    }
     Ok(())
 }
