5b16426ef2ca0f9677dbe95f02f67c5fb1de255b
Jeff Zhao <jeff.no.zhao@gmail.com>
2021-10-15 18:40:08 -0400

    add support for going into subdirectories via fzf

--- src/commands/subdir_fzf.rs
+++ src/commands/subdir_fzf.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::io::{BufReader, BufWriter, Read, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
 use crate::commands::cursor_move;
@@ -45,20 +45,48 @@ pub fn subdir_fzf(context: &mut AppContext, backend: &mut TuiBackend) -> Joshuto
     }
     let fzf_output = fzf.wait_with_output();
 
-    backend.terminal_restore()?;
-
-    if let Ok(output) = fzf_output {
-        if output.status.success() {
+    match fzf_output {
+        Ok(output) if output.status.success() => {
             if let Ok(selected) = std::str::from_utf8(&output.stdout) {
                 let path: PathBuf = PathBuf::from(selected);
-                if path.is_dir() {
-                    change_directory(context, &path)?;
-                } else if let Some(parent) = path.parent() {
-                    change_directory(context, &parent)?;
-                }
+                fzf_change_dir(context, path.as_path())?;
+            }
+        }
+        _ => {}
+    }
+
+    backend.terminal_restore()?;
+
+    Ok(())
+}
+
+pub fn fzf_change_dir(context: &mut AppContext, path: &Path) -> JoshutoResult<()> {
+    if path.is_dir() {
+        change_directory(context, &path)?;
+    } else if let Some(parent) = path.parent() {
+        let file_name = path
+            .file_name()
+            .and_then(|name| name.to_str())
+            .unwrap()
+            .trim();
+
+        change_directory(context, &parent)?;
+
+        let index = match context.tab_context_ref().curr_tab_ref().curr_list_ref() {
+            Some(curr_list) => curr_list
+                .iter()
+                .enumerate()
+                .find(|(i, e)| e.file_name() == file_name)
+                .map(|(i, e)| i),
+            None => None,
+        };
+        eprintln!("{:?}", index);
+
+        if let Some(index) = index {
+            if let Some(curr_list) = context.tab_context_mut().curr_tab_mut().curr_list_mut() {
+                curr_list.index = Some(index);
             }
         }
     }
-
     Ok(())
 }
