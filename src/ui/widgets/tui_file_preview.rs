e83fbdbfcf8f9fe1cdad9adc9c09e30d816f2d24
Noah Too <krivahtoo@gmail.com>
2022-06-22 10:00:09 +0300

    Bump `ansi-to-tui` to `0.6` and remove `tui_old`
    `ansi-to-tui` 0.6 uses the latest `tui`

--- src/ui/widgets/tui_file_preview.rs
+++ src/ui/widgets/tui_file_preview.rs
@@ -1,45 +1,9 @@
 use tui::buffer::Buffer;
 use tui::layout::Rect;
-use tui::style::{Color, Style};
-use tui::text::{Span, Spans};
+use tui::text::Span;
 use tui::widgets::Widget;
 
 use crate::preview::preview_file::FilePreview;
-
-fn color_to_color(color: tui_old::style::Color) -> Color {
-    match color {
-        tui_old::style::Color::Reset => Color::Reset,
-        tui_old::style::Color::Black => Color::Black,
-        tui_old::style::Color::Red => Color::Red,
-        tui_old::style::Color::Green => Color::Green,
-        tui_old::style::Color::Yellow => Color::Yellow,
-        tui_old::style::Color::Blue => Color::Blue,
-        tui_old::style::Color::Magenta => Color::Magenta,
-        tui_old::style::Color::Cyan => Color::Cyan,
-        tui_old::style::Color::Gray => Color::Gray,
-        tui_old::style::Color::DarkGray => Color::DarkGray,
-        tui_old::style::Color::LightRed => Color::LightRed,
-        tui_old::style::Color::LightGreen => Color::LightGreen,
-        tui_old::style::Color::LightYellow => Color::LightYellow,
-        tui_old::style::Color::LightBlue => Color::LightBlue,
-        tui_old::style::Color::LightMagenta => Color::LightMagenta,
-        tui_old::style::Color::LightCyan => Color::LightCyan,
-        tui_old::style::Color::White => Color::White,
-        tui_old::style::Color::Rgb(r, g, b) => Color::Rgb(r, g, b),
-        tui_old::style::Color::Indexed(i) => Color::Indexed(i),
-    }
-}
-
-fn style_to_style(style: tui_old::style::Style) -> Style {
-    let mut new_style = Style::default();
-    if let Some(fg) = style.fg {
-        new_style = new_style.fg(color_to_color(fg));
-    }
-    if let Some(bg) = style.bg {
-        new_style = new_style.fg(color_to_color(bg));
-    }
-    new_style
-}
 
 pub struct TuiFilePreview<'a> {
     preview: &'a FilePreview,
@@ -77,19 +41,6 @@ impl<'a> TuiFilePreview<'a> {
                     .skip(self.preview.index)
                     .zip(area.y..area.y + area.height)
                 {
-                    // required to convert between different tui-rs versions.
-                    // remove once ansi-to-tui depends on latest tui-rs
-                    let span_vec: Vec<Span> = line
-                        .0
-                        .iter()
-                        .map(|s| Span {
-                            content: s.content.clone(),
-                            style: style_to_style(s.style),
-                        })
-                        .collect();
-                    let spans = Spans(span_vec);
-                    let line = &spans;
-
                     buf.set_spans(area.x, y, line, area.width);
                 }
             }
