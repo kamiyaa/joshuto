2c190b7cefa48e7bfbf0761a4eb60d36ad72cd0a
Cameron Eldridge <cameldridge@gmail.com>
2022-07-13 13:34:11 -0400

    Remove commented code

--- src/key_command/command_keybind.rs
+++ src/key_command/command_keybind.rs
@@ -8,12 +8,3 @@ pub enum CommandKeybind {
     SimpleKeybind(HashMap<Option<FileType>, Command>),
     CompositeKeybind(KeyMapping),
 }
-
-// impl std::fmt::Display for CommandKeybind {
-//     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-//         match self {
-//             CommandKeybind::SimpleKeybind(s) => write!(f, "{}", s),
-//             CommandKeybind::CompositeKeybind(_) => write!(f, "..."),
-//         }
-//     }
-// }
