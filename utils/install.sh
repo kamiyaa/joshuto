531aaf3235f94d4a9eaefd7b5b288b781c942fb7
kennycallado <kennycallado@hotmail.com>
2023-09-01 22:12:30 +0200

    ALSO install

--- utils/install.sh
+++ utils/install.sh
@@ -13,21 +13,18 @@ set -eo pipefail
 # Hardcoded version is used for now
 declare -r HARDCODED_VERSION="v0.9.5"
 
-declare -r REPO="${REPO:-"kamiyaa/joshuto"}"
-# declare -r OS="$(uname -s)"
-declare -r OS="Darwin"
+declare -r OS="$(uname -s)"
 declare -r ARCH="$(uname -m)"
+declare -r REPO="${REPO:-"kamiyaa/joshuto"}"
 declare -r INSTALL_PREFIX="${INSTALL_PREFIX:-"$HOME/.local/bin"}"
 declare -r RELEASE_VER="${RELEASE_VER:-"$HARDCODED_VERSION"}"
 
 declare ARCHIVE_NAME
-
 if [ "$OS" == "Linux" ]; then
   # TODO: add support for musl
   ARCHIVE_NAME="joshuto-$RELEASE_VER-$ARCH-unknown-linux-gnu"
 elif [ "$OS" == "Darwin" ]; then
-  # ARCHIVE_NAME="joshuto-$RELEASE_VER-$ARCH-apple-darwin"
-  ARCHIVE_NAME="joshuto-v0.9.5-x86_64-apple-darwin"
+  ARCHIVE_NAME="joshuto-$RELEASE_VER-$ARCH-apple-darwin"
 else
   echo "$OS platform is not supported currently"
   exit 1
@@ -41,11 +38,11 @@ fi
 
 declare -r ARCHIVE_URL="https://github.com/$REPO/releases/download/$RELEASE_VER/$ARCHIVE_NAME.tar.gz"
 declare -r CHECKSUM_URL="$ARCHIVE_URL.sha256sum"
-echo $ARCHIVE_URL
+# echo $ARCHIVE_URL
 
 declare -r DOWNLOAD_DIR="$(mktemp -d)"
 declare -r RELEASE_SHA="$(curl -Ls "$CHECKSUM_URL" | awk '{print $1}')"
-echo $RELEASE_SHA
+# echo $RELEASE_SHA
 
 function main() {
   download_archive
@@ -55,17 +52,13 @@ function main() {
 
 function download_archive() {
   echo "Downloading joshuto's binary from $RELEASE_VER release.."
-  if ! curl --progress-bar --fail -L "$ARCHIVE_URL" -o "$DOWNLOAD_DIR/$ARCHIVE_NAME"; then
+  if ! curl --progress-bar --fail -L "$ARCHIVE_URL" -o "$DOWNLOAD_DIR/$ARCHIVE_NAME.tar.gz"; then
     echo "Download failed.  Check that the release/filename are correct."
     exit 1
   fi
   echo "Download complete!"
 }
 
-# Verification is disabled for now
-#
-# Action automated-build should generate a checksum
-# archive on release to enable verifications
 function verify_archive() {
   echo "Verifying the installation..."
   DOWNLOADED_SHA="$(openssl dgst -sha256 "$DOWNLOAD_DIR/$ARCHIVE_NAME.tar.gz" | awk '{print $2}')"
@@ -83,7 +76,7 @@ function install_joshuto() {
   echo "Installing joshuto..."
 
   pushd "$DOWNLOAD_DIR"
-  tar -xzf "$DOWNLOAD_DIR/$ARCHIVE_NAME"
+  tar -xzf "$DOWNLOAD_DIR/$ARCHIVE_NAME.tar.gz"
   popd
 
   if [ ! -d "$DOWNLOAD_DIR/$ARCHIVE_NAME" ]; then
