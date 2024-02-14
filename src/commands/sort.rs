971f53e42e0bfd16248d71e4bc84b29a4ef8689d
Akmadan23 <azadahmadi@mailo.com>
2024-02-14 22:48:47 +0100

    fix: update all lists viewports on sort method change

--- src/commands/sort.rs
+++ src/commands/sort.rs
@@ -25,5 +25,22 @@ pub fn toggle_reverse(context: &mut AppContext) -> AppResult {
 
 fn refresh(context: &mut AppContext) -> AppResult {
     reload::soft_reload_curr_tab(context)?;
+
+    let ui_context = context.ui_context_ref().clone();
+    let display_options = context.config_ref().display_options_ref().clone();
+    let curr_tab = context.tab_context_mut().curr_tab_mut();
+
+    macro_rules! update_viewport {
+        ($x_list_mut: ident) => {
+            if let Some(list) = curr_tab.$x_list_mut() {
+                list.update_viewport(&ui_context, &display_options);
+            }
+        };
+    }
+
+    update_viewport!(curr_list_mut);
+    update_viewport!(parent_list_mut);
+    update_viewport!(child_list_mut);
+
     Ok(())
 }
